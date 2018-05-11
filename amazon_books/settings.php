bd4a842cb446ddc397c9ab941746d870009f373a
Sean Chalmers <seandchalmers@yahoo.ca>
2018-05-11 11:16:09 -0400

    Add code base

---
+++ amazon_books/settings.php
@@ -0,0 +1,6 @@
+<?php
+define('AWS_API_KEY', 'AKIAINMQATUGYM2WDFLA');
+define('AWS_API_SECRET_KEY', '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');
+define('AWS_ASSOCIATE_TAG', 'nomad20-20');
+
+define('AMAZON_BUY_BUTTON_TEXT', 'Buy From Amazon');
