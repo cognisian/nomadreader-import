3fbd7464842792469b889aa4214f83a013f0924b
Sean Chalmers <seandchalmers@yahoo.ca>
2018-05-18 11:35:03 -0400

    Remove previous ref to amazon-books

--- nomadreader-books/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('AWS_API_KEY', 'AKIAINMQATUGYM2WDFLA');
-define('AWS_API_SECRET_KEY', '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');
-define('AWS_ASSOCIATE_TAG', 'nomad20-20');
-
-define('AMAZON_BUY_BUTTON_TEXT', 'Buy From Amazon');
